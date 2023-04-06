8f578b4dc2efade257156353b4b7dfceeca1adb2
jaeleeps <jaeleeps@gmail.com>
2023-04-05 23:59:16 -0400

    init MongoDB Atlas Initialization Test

--- __test__/CrudifyClient.test.ts
+++ __test__/CrudifyClient.test.ts
@@ -4,6 +4,8 @@ import { Bucket } from '../src/Bucket/Bucket';
 import { testFirebaseConfig } from './env/tesFirebaseConfig';
 import firebase from 'firebase';
 import initializeApp = firebase.initializeApp;
+import { MongoClient } from 'mongodb';
+import { coneectionURI, testMongoDBAtlasPassword } from './env/testMongoDBAtlasConfig';
 test("Singleton Test", () => {
   const instance_a = CrudifyClient.getInstance();
   const instance_b = CrudifyClient.getInstance();
@@ -27,4 +29,19 @@ test("Firebase Initialization Test", () => {
 // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   console.log(app);
+  expect(app).not.toBe(null);
+})
+
+test("MongoDB Atlas Initialization Test", async () => {
+  const password: string = testMongoDBAtlasPassword;
+  const coneectionURI: string = `mongodb+srv://jaeleeps:${password}@cluster0.cfhx0ec.mongodb.net/?retryWrites=true&w=majority`;
+  const client = new MongoClient(coneectionURI);
+  await client.connect();
+
+  const databasesList = await client.db().admin().listDatabases();
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+
+  expect(client).not.toBe(null);
+  expect(databasesList).not.toBe(null);
 })
