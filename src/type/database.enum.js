117b01c73d9dd4c313521a2969fd4c3b5024e65a
Catherine Xie <catx@lawn-128-61-25-178.lawn.gatech.edu>
2023-04-19 22:29:46 -0400

    Extra js files deleted

--- src/type/database.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DatabaseType = void 0;
-var DatabaseType;
-(function (DatabaseType) {
-    DatabaseType["Mongo"] = "MONGO";
-    DatabaseType["Firestore"] = "Firestore";
-})(DatabaseType = exports.DatabaseType || (exports.DatabaseType = {}));
